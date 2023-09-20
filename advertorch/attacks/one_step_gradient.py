c0af831e584ac2cab723c089fa3fff33cf3d8300
Jue WANG <zjuwangjue@gmail.com>
2023-09-20 17:02:37 +0800

    Update one_step_gradient.py

--- advertorch/attacks/one_step_gradient.py
+++ advertorch/attacks/one_step_gradient.py
@@ -10,6 +10,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import torch
 import torch.nn as nn
 
 from advertorch.utils import clamp
